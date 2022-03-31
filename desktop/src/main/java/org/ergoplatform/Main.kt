6a7f5e0e0d36e9d2fc44df9824b52ce68fd96bb0
Benjamin Schulte <bs@golfgl.de>
2022-03-31 10:03:35 +0200

    AlertDialog/AnimatedVisibility stash

--- desktop/src/main/java/org/ergoplatform/Main.kt
+++ desktop/src/main/java/org/ergoplatform/Main.kt
@@ -1,10 +1,19 @@
 package org.ergoplatform
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.arkivanov.decompose.DefaultComponentContext
-import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.jetbrains.lifecycle.LifecycleController
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.badlogic.gdx.utils.I18NBundle
@@ -13,12 +22,13 @@ import org.ergoplatform.desktop.ui.DecomposeDesktopExampleTheme
 import org.ergoplatform.desktop.ui.navigation.NavHostComponent
 import org.ergoplatform.uilogic.STRING_APP_NAME
 
-@OptIn(ExperimentalDecomposeApi::class)
+@ExperimentalAnimationApi
+@ExperimentalMaterialApi
 fun main() {
     val lifecycle = LifecycleRegistry()
     val root = NavHostComponent(DefaultComponentContext(lifecycle = lifecycle))
-
     application {
+
         val windowState = rememberWindowState()
         LifecycleController(lifecycle, windowState)
 
@@ -30,7 +40,27 @@ fun main() {
             title = Application.texts.get(STRING_APP_NAME)
         ) {
             DecomposeDesktopExampleTheme {
+                var show by remember { mutableStateOf(true) }
+
                 root.render()
+                AnimatedVisibility(visible = show)
+                {
+                    AlertDialog(onDismissRequest = {},
+                        title = {
+                            Text("A title")
+                        },
+                        text = {
+                            Button(onClick = {}) {
+                                Text("A button")
+                            }
+                            Text("A text")
+                        },
+                        confirmButton = {
+                            Button(onClick = { show = false }) {
+                                Text("A button")
+                            }
+                        })
+                }
             }
         }
     }
